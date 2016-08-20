397de4db8a7c03c963e5d281a9ac5635752675c7
dxx <dxx@gmail.com>
2016-08-20 11:15:12 +0800

    加入显示天气信息的功能

--- src/com/coolweather/app/util/HttpUtil.java
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.util.Log;
+
 public class HttpUtil {
 	public static void sentHttpRequst(final String address, 
 			  final HttpCallbackListener listener) {
@@ -19,18 +21,27 @@ public class HttpUtil {
 					connection.setRequestMethod("GET");
 					connection.setConnectTimeout(8000);
 					connection.setReadTimeout(8000);
+					
 					InputStream in = connection.getInputStream();
 					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 					StringBuilder respose = new StringBuilder();
-					String line;
-					while ((line = reader.readLine()) != null) {
-						respose.append(line);
+					String line = null;
+					
+					line = reader.readLine();
+					Log.d("MainActivity", line + " wwww");
+					
+					while ( line != null) {       
+						 respose.append(line);    
+						 line = reader.readLine();
+						 Log.d("MainActivity", line + " www");
 					}
+					
 					if (listener != null) {
 						//回调onFinish()
 						listener.onFinish(respose.toString());
 					}
 				} catch (Exception e) {
+					Log.e("MainActivity", Log.getStackTraceString(e));
 					if (listener != null) {
 						//回调onError()方法
 						listener.onError(e);
